556e880a269c19bcb0d6a9bdc644d9b8262ceea4
Rafhael <rafhael.dev@gmail.com>
2023-10-16 13:31:23 -0300

    feat: authorization server flow with password credentials and opaque tokens

--- src/main/java/com/studies/foodorders/auth/AuthorizationServerConfig.java
+++ src/main/java/com/studies/foodorders/auth/AuthorizationServerConfig.java
@@ -1,9 +1,38 @@
 package com.studies.foodorders.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
-public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {}
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("web-client")
+                .secret(passwordEncoder.encode("1234"))
+                .authorizedGrantTypes("password")
+                .scopes("write", "read")
+                .accessTokenValiditySeconds(60 * 60 * 6); // 6 hours (default is 12 hours)
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.authenticationManager(authenticationManager);
+    }
+
+}
