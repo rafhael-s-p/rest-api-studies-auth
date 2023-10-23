cb3961bc6d6d0d9dbd70fb243f213233c340cac1
Rafhael <rafhael.dev@gmail.com>
2023-10-23 08:05:58 -0300

    feat: configuring symmetric key

--- src/main/java/com/studies/foodorders/auth/AuthorizationServerConfig.java
+++ src/main/java/com/studies/foodorders/auth/AuthorizationServerConfig.java
@@ -3,7 +3,6 @@ package com.studies.foodorders.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,9 +13,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import java.util.Arrays;
 
@@ -102,7 +99,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-        jwtAccessTokenConverter.setSigningKey("MySigningKey");
+        jwtAccessTokenConverter.setSigningKey("OCUHYglYq2/0txtRaV+WGuqN7CLuCMnf6DpTTFgaUHP0vFa+24Y/OvcKRbQFWWE5");
 
         return jwtAccessTokenConverter;
     }
