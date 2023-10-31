deb161a5a43fa4592ab98fe11faab29e2574e65f
Rafhael <rafhael.dev@gmail.com>
2023-10-31 09:07:21 -0300

    feat: configuring oauth2 clients in a sql database

--- src/main/java/com/studies/foodorders/auth/core/AuthorizationServerConfig.java
+++ src/main/java/com/studies/foodorders/auth/core/AuthorizationServerConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -21,14 +20,12 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
+import javax.sql.DataSource;
 import java.util.Arrays;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -39,54 +36,12 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private JwtKeyStoreProperties jwtKeyStoreProperties;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients
-                .inMemory()
-                    .withClient("web-client")
-                    .secret(passwordEncoder.encode("1234"))
-                    .authorizedGrantTypes("password", "refresh_token")
-                    .scopes("WRITE", "READ")
-                    .accessTokenValiditySeconds(60 * 60 * 6) // 6 hours (default is 12 hours)
-                    .refreshTokenValiditySeconds(7 * 24 * 60 * 60) // 7 days
-
-                .and()
-                    .withClient("any-back-end-application")
-                    .secret(passwordEncoder.encode("1234"))
-                    .authorizedGrantTypes("client_credentials")
-                    .scopes("WRITE", "READ")
-                /*
-                * It must request a code by the url
-                * http://localhost:8081/oauth/authorize?response_type=code&client_id=client-oauth2-authorization-code&state=abcd&redirect_uri=http://localhost:8082
-                *
-                * PKCE Plain
-                * http://localhost:8081/oauth/authorize?response_type=code&client_id=client-oauth2-authorization-code&redirect_uri=http://localhost:8082&code_challenge=ABcd&code_challenge_method=plain
-                *
-                * PKCE SHA-256
-                * http://localhost:8081/oauth/authorize?response_type=code&client_id=client-oauth2-authorization-code&redirect_uri=http://localhost:8082&code_challenge=F4BP93803TyWD7lruWDpi3BDrPuIYFUlaboOSVCANiM&code_challenge_method=s256
-                *
-                * Link to generate Code Verifier and Code Challenge
-                * https://tonyxu-io.github.io/pkce-generator/
-                *
-                * */
-                .and()
-                    .withClient("client-oauth2-authorization-code")
-                    .secret(passwordEncoder.encode("1234"))
-                    .authorizedGrantTypes("authorization_code")
-                    .scopes("WRITE", "READ")
-                    .redirectUris("http://localhost:8082")
-                /*
-                 * It must request a code by the url
-                 * http://localhost:8081/oauth/authorize?response_type=token&client_id=implicit-grant-type-user&state=abcd&redirect_uri=http://localhost:8082
-                 * */
-                .and()
-                    .withClient("implicit-grant-type-user")
-                    .authorizedGrantTypes("implicit")
-                    .scopes("WRITE", "READ")
-                    .redirectUris("http://localhost:8082")
-                .and()
-                    .withClient("checktoken")
-                    .secret(passwordEncoder.encode("check1234"));
+        clients.jdbc(dataSource);
     }
 
     @Override
